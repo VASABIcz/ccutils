6ad6e4b3ef0a0fb619b40d58196a574a154cf7ff
vasabi <vasabisroa@gmail.com>
2026-01-08 12:42:48 +0100

    Add BetterSpan - supports construction from vector/span/initializer list

---
+++ utils/BetterSpan.h
@@ -0,0 +1,27 @@
+#include <span>
+#include <vector>
+
+template<typename T>
+struct BetterSpan {
+    std::span<T> inner;
+
+    BetterSpan(std::span<T> s) {
+        inner = std::span<T>(s.begin(), s.end());
+    }
+
+    BetterSpan(std::initializer_list<T> s) {
+        inner = std::span<T>((T*)s.begin(), (T*)(s.end()));
+    }
+
+    BetterSpan(std::vector<T>& s) {
+        inner = std::span<T>(s.begin(), s.end());
+    }
+
+    auto begin() {
+        return inner.begin();
+    }
+
+    auto end() {
+        return inner.end();
+    }
+};
