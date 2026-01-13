dfc19f3b879560f28ca3bd7239f25daa22ace94d
vasabi <vasabisroa@gmail.com>
2026-01-13 09:01:00 +0100

    Fix live overlapping ranges Improve formating

--- codegen/x86/ir/print_utils.h
+++ codegen/x86/ir/print_utils.h
@@ -13,47 +13,64 @@
 #include <chrono>
 #include <cstddef>
 
-static void printBlockInst(X86Instruction* inst, std::ostream& ss) {
-    if (inst->defs.size() == 1) {
-        ss << inst->defs.front()->toString();
+inline void formatDst(X86Instruction* inst, std::ostream& ss) {
+    if (inst->defCount() == 0) {
+        ss << "()";
+    } else if (inst->defCount() == 1) {
+        ss << inst->getDef(0)->toString();
     } else {
-        ss << "[";
-        for (auto def: inst->defs) {
-            ss << def->toString() << ",";
+        ss << "(";
+        for (auto i = 0u; i < inst->defCount(); i++) {
+            ss << inst->getDef(i)->toString() << ",";
         }
-        ss << "]";
+        ss << ")";
+    }
+}
+
+inline void printBlockInst(X86Instruction* inst, std::ostream& ss) {
+    std::stringstream buffer;
+    formatDst(inst, buffer);
+    if (buffer.str().size() < 3) {
+        auto size = buffer.str().size();
+        repeat1(3-size) {
+            buffer << "⠀";
+        }
     }
 
-    ss << " := ";
+    ss << buffer.str();
+
+    ss << "⠀= ";
 
     ss << inst->className();
-    ss << " [";
-    for (auto i = 0ul; i < inst->uses.size(); i++) {
-        ss << inst->uses[i]->toString();
-        if (i != inst->uses.size() - 1) ss << ",";
+
+    ss << " ";
+
+    for (auto i = 0ul; i < inst->useCount(); i++) {
+        ss << inst->getUse(i)->toString();
+        if (i != inst->useCount() - 1) ss << ",";
     }
-    ss << "]";
+    ss << "⠀⠀⠀⠀⠀";
 }
 
 static void printBlock(Block* b) {
     for (auto inst: b->iterator()) {
-        if (inst->defs.size() == 1) {
-            std::cout << inst->defs.front()->toString();
+        if (inst->defCount() == 1) {
+            std::cout << inst->getDef(0)->toString();
         } else {
             std::cout << "[";
-            for (auto def: inst->defs) {
-                std::cout << def->toString() << ",";
+            for (auto i = 0u; i < inst->defCount(); i++) {
+                std::cout << inst->getDef(i)->toString() << ",";
             }
             std::cout << "]";
         }
 
-        std::cout << " := ";
+        std::cout << " = ";
 
         std::cout << inst->className();
         std::cout << " [";
-        for (auto i = 0ul; i < inst->uses.size(); i++) {
-            std::cout << inst->uses[i]->toString();
-            if (i != inst->uses.size() - 1) std::cout << ",";
+        for (auto i = 0ul; i < inst->useCount(); i++) {
+            std::cout << inst->getUse(i)->toString();
+            if (i != inst->useCount() - 1) std::cout << ",";
         }
         std::cout << "]";
         std::cout << std::endl;
--- codegen/x86/ir/ranges.hpp
+++ codegen/x86/ir/ranges.hpp
@@ -10,6 +10,17 @@ struct Range {
     X86Instruction* first;
     X86Instruction* last;
 
+    size_t getLength() {
+        size_t i = 1;
+        auto cur = first;
+        while (cur != last) {
+            cur = cur->next;
+            i++;
+        }
+
+        return i;
+    }
+
     Range merge(Range other) {
         auto start = MIN_BY(this->first, other.first, ->orderId);
         auto end = MAX_BY(this->last, other.last, ->orderId);
@@ -24,15 +35,100 @@ struct Range {
     bool intersects(Range other) { return Range::intersects(other, *this) || Range::intersects(*this, other); }
 };
 
-struct RangeSet {
-    std::vector<Range> ranges;
+struct ListNode {
+    ListNode* next = nullptr;
+    Range range;
+};
 
-    void add(Range r) { ranges.push_back(r); }
+struct List {
+    ListNode* root = nullptr;
+
+    struct Iter {
+        ListNode* n;
+
+        Range& operator*() const {
+            return n->range;
+        }
+
+        void operator++() {
+            n = n->next;
+        }
+
+        bool operator==(const Iter& other) const { return this->n == other.n; }
+    };
+
+    auto begin() {
+        return Iter{root};
+    }
+
+    auto end() {
+        return Iter{nullptr};
+    }
+
+    void add(Range r) {
+        root = new ListNode(root, r);
+    }
+
+    void forEach(auto fn) {
+        auto cur = root;
+        while (cur) {
+            auto n = cur->next;
+            fn(cur);
+            cur = n;
+        }
+    }
+
+    void remove(ListNode* node, ListNode* prev) {
+        if (prev == nullptr) {
+            root = node->next;
+        } else {
+            prev->next = node->next;
+        }
+        delete node;
+    }
+
+    void addRang(Range newRange) {
+        ListNode* prev = nullptr;
+        forEach([&](ListNode* it) {
+            auto r1 = it->range;
+            if (it->range.intersects(newRange)) {
+                newRange = r1.merge(newRange);
+                remove(it, prev);
+            }
+            prev = it;
+        });
+        add(newRange);
+    }
+};
+
+struct RangeSet {
+    List list;
+
+    void add(Range r) {
+        list.addRang(r);
+    }
 
     bool intersects(Range other) {
-        for (auto r: ranges) {
+        for (auto r : list) {
             if (r.intersects(other)) return true;
         }
         return false;
+    }
+
+    bool intersects(RangeSet other) {
+        for (auto a : list) {
+            for (auto b : other.list) {
+                if (a.intersects(b)) return true;
+            }
+        }
+        return false;
+    }
+
+    std::optional<Range> getStart(X86Instruction* inst) {
+        for (auto range : list) {
+            if (range.first == inst) return range;
+        }
+
+        return std::nullopt;
     }
 };
